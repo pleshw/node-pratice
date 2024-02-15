09df58f006d9d9165712ca54b5b22805f29eb248
pleshw <pleshw@outlook.com>
2024-02-15 16:35:34 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test");
+console.log('test');
+(0, test_1.test)();
---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+function test() {
+    console.log('dsdsadas');
+}
+exports.test = test;
